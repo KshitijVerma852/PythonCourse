668eb87f7dc3d25b812d3bef5e5fae950652a92c
Kshitij <thisisdinku@outlook.com>
2020-09-07 00:35:41 +0100

    delete not working

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -1,20 +1,42 @@
 class Node {
-    constructor(prev = null, data, next = null) {
-        this.prev = prev;
-        this.data = data;
+    constructor(value, next, prev) {
+        this.value = value;
         this.next = next;
+        this.prev = prev;
     }
 }
-
 class LinkedList {
     constructor(value) {
         this.head = {
-            prev: null,
-            data: value,
+            value: value,
             next: null,
+            prev: null,
         };
-        this.length = 1;
         this.tail = this.head;
+        this.length = 1;
+    }
+    show() {
+        let ans = [];
+        let currentNode = this.head;
+        while (currentNode != null) {
+            ans.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return ans;
+    }
+    append(value) {
+        const newNode = new Node(value);
+        this.tail.next = newNode;
+        newNode.prev = this.tail;
+        this.tail = newNode;
+        this.length++;
+    }
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.head = newNode;
+        this.length++;
     }
     traverseToIndex(index) {
         let counter = 0;
@@ -25,33 +47,76 @@ class LinkedList {
         }
         return currendNode;
     }
-    append(localData) {
-        const newNode = new Node(null, localData, null);
-        this.tail.next = newNode;
-        newNode.prev = this.tail;
-
-        this.tail = newNode;
+    insertToIndex(index, value) {
+        const prevNode = this.traverseToIndex(index - 1);
+        const afterNode = this.traverseToIndex(index + 1);
+        const newNode = new Node(value);
+        prevNode.next = newNode;
+        newNode.prev = prevNode;
+        newNode.next = afterNode;
+        afterNode.prev = newNode;
         this.length++;
     }
-    show() {
-        let ans = [];
-        let currentNode = this.head;
-        while (currentNode != null) {
-            ans.push(currentNode.data);
-            currentNode = currentNode.next;
+    legalIndex(index) {
+        if (index < 0 || index >= this.length) {
+            return false;
+        } else {
+            return true;
         }
-        return ans;
     }
-    insert(data, index) {
-        if (index >= this.length) {
-            
+    deleteAtIndex(index) {
+        if (this.legalIndex(index)) {
+            if (index == 0) {
+                const afterNode = this.traverseToIndex(index + 1);
+                this.head.next = null;
+                afterNode.prev = null;
+                this.head = afterNode;
+                this.length--;
+            } else if (index == this.length - 1) {
+                const beforeNode = this.traverseToIndex(index - 1);
+                beforeNode.next = null;
+                this.tail.prev = null;
+                this.tail = beforeNode;
+                this.length--;
+            } else {
+                const beforeNode = this.traverseToIndex(index - 1);
+                const afterNode = this.traverseToIndex(index + 1);
+                const delNode = this.traverseToIndex(index);
+                delNode.next = null;
+                delNode.prev = null;
+                beforeNode.next = afterNode;
+                afterNode.prev = beforeNode;
+                this.length--;
+            }
+        } else {
+            console.log("Invalid Index");
         }
+    }
+    deleteAllNodeWithValue(value) {
+        let currentNode = this.head;
+        let counter = 0;
+        while (currentNode != null) {
+            if (currentNode.value == value) {
+                this.deleteAtIndex(counter);
+                continue;
+            }
+            currentNode = currentNode.next;
+            counter++;
+        }
+        this.length -= counter;
     }
 }
 
-const llist1 = new LinkedList(1);
+llist1 = new LinkedList(1);
 llist1.append(2);
 llist1.append(3);
 llist1.append(4);
-const llist1Show = llist1.show();
-console.log(llist1Show);
+llist1.prepend(10);
+llist1.insertToIndex(2, 100);
+llist1.deleteAtIndex(2);
+llist1.append(1);
+llist1.append(1);
+llist1.append(1);
+llist1.deleteAllNodeWithValue(1);
+const ans = llist1.show();
+console.log(ans);
